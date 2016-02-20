2a9bb96314a778ea15394a0258ed797989f8d23f
sudeep <sudeip@gmail.com>
2016-02-20 08:44:25 -0600

    Initial Commit

---
+++ Sajusma/DataViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataViewController.h
+//  Sajusma
+//
+//  Created by Sudeep Jain on 2/20/16.
+//  Copyright © 2016 Sudeep Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
+
---
+++ Sajusma/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Sajusma
+//
+//  Created by Sudeep Jain on 2/20/16.
+//  Copyright © 2016 Sudeep Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
